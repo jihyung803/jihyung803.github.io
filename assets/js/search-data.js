d9d0f80f645494b61c78bbef13e2b7ec5acc4df9
jihyung803 <50003380+jihyung803@users.noreply.github.com>
2025-12-22 17:31:01 +0000

    Deploying to gh-pages from @ jihyung803/jihyung803.github.io@13f084e823ebcf09b9aba5e64057b69846f110d8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,7 +508,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%69%68%79%75%6E%67%38%30%33@%75%74%65%78%61%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
