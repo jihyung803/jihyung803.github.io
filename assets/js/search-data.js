d09ec85978e42f579e57bfcf1b50065f4ac8adc5
jihyung803 <50003380+jihyung803@users.noreply.github.com>
2026-06-17 05:23:15 +0000

    Deploying to gh-pages from @ jihyung803/jihyung803.github.io@7ce13b95ccbda6a3a50109bdd4e5d31dba09ae6c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,9 +16,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "latest curriculum vitae.",
+        },{id: "nav-resume",
+          title: "Resume",
+          description: "latest resume.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
